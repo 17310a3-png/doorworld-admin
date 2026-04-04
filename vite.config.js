225fc74966f739e921594cf0b7da13d23dedeb90
ttdwooden <ttdwooden@gmail.com>
2026-04-04 17:10:46 +0800

    fix: update deploy config for doorworld-admin repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/doorworld-admin-react/',
+  base: '/doorworld-admin/',
   build: {
     outDir: 'dist'
   }
